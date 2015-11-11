d407562793fd692ff6759cd047e59b4a95f5f594
carlosinftech <carlos.inf.tech@gmail.com>
2015-11-11 15:15:24 -0500

    Fix file format

--- DesignPatterns/src/visitor/DegradationCalculator.java
+++ DesignPatterns/src/visitor/DegradationCalculator.java
@@ -1,13 +1,11 @@
 package visitor;
-<<<<<<< HEAD
+
 /**
  * @author Carlos Perez carlosinftech
  * @version 1.0
  * 
  */
-=======
 
->>>>>>> 069268fef778626762a5da55706fe469fe8e129b
 public class DegradationCalculator implements Visitor{
 
 	@Override
